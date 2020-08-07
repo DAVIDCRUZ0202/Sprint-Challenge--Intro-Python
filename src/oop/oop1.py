8dee402257d4a68e768311cbb2518e68df78037d
David Cruz <dcman1211@gmail.com>
2020-08-07 12:37:47 -0400

    Solid 2 passing state

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,36 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+class Vehicle:
+    #This is the base class for all other classes   
+    pass
+
+class FlightVehicle(Vehicle):
+    # This is a child class to the vehicle class.
+    pass
+
+class Starship(FlightVehicle):
+    # This is a child class to the FlightVehicle class.
+    pass
+
+class Airplane(FlightVehicle):
+    # This is child class to the FlightVehicle class.
+    pass
+
+class GroundVehicle(Vehicle):
+    # This is a child class to the Vehicle class.
+    pass
+
+class Car(GroundVehicle):
+    # Child class to GroundVehicle.
+    pass
+
+class Motorcycle(GroundVehicle):
+    # Child class to GroundVehicle.
+    pass
+
+# Notes on Class Inheritance
+# Classes will always follow a parent-child heirarchy. If a class is inheriting from a different class, it also
+# inherits all aspects of the original parent class. This is an explanation for why the class Motorcycle would
+# inherit all qualities of the GroundVehicle class as well as all quallities of the original parent class, Vehicle.
